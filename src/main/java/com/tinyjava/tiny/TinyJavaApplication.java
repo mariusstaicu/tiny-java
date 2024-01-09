4ed3ee75a8da0d367d87a3dfeb53c1bd00076e18
Marius Staicu <marius.s.staicu@gmail.com>
2024-01-09 13:01:45 +0200

    start.spring.io initial files

---
+++ src/main/java/com/tinyjava/tiny/TinyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tinyjava.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyjava/tiny/TinyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyjava.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
