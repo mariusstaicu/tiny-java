b1016a9e11284f07affc13ae305d2ed0b24d9b59
Marius Staicu <marius.staicu@esolutions.ro>
2024-01-10 11:49:05 +0200

    - jlink

---
+++ build-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./mvnw clean install
+
+docker build -f Dockerfile-tiny-ubuntu -t tiny-ubuntu .
+docker build -f Dockerfile-tiny-openjdk -t tiny-openjdk .
+docker build -f Dockerfile-tiny-amazoncoretto -t tiny-amazoncoretto .
+docker build -f Dockerfile-tiny-amazoncoretto-alpine -t tiny-amazoncoretto-alpine .
+docker build -f Dockerfile-tiny-temurin -t tiny-temurin .
+docker build -f Dockerfile-tiny-temurin-alpine -t tiny-temurin-alpine .
+docker build -f Dockerfile-tiny-temurin-jre -t tiny-temurin-jre .
+docker build -f Dockerfile-tiny-liberica-debian -t tiny-liberica-debian .
+docker build -f Dockerfile-tiny-liberica-alpine -t tiny-liberica-alpine .
+docker build -f Dockerfile-tiny-distroless -t tiny-distroless .
+docker build -f Dockerfile-tiny-liberica-runtime-container-glibc -t tiny-runtime-glibc .
+docker build -f Dockerfile-tiny-liberica-runtime-container-musl -t tiny-runtime-musl .
+docker build -f Dockerfile-tiny-jlink-distroless-java-base -t tiny-distroless-base.
+docker build -f Dockerfile-tiny-jlink-liberica-runtime-container-glibc -t tiny-alpaquita-base-glibc .
+docker build -f Dockerfile-tiny-jlink-liberica-runtime-container-musl -t tiny-alpaquita-base-musl .
+
--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 ```bash
 docker images --format "{{.ID}}\t{{.Size}}\t{{.Repository}}" | sort -k 2 -h
 ```
+
+
+### Articles
+
+- Are distroless containers a perfect solution ? [Article](https://bell-sw.com/blog/distroless-containers-for-security-and-size/)
+- Best linux distribution for server and cloud [Article](https://bell-sw.com/announcements/2022/06/29/linux-distributions-for-server-and-cloud-overview/)
